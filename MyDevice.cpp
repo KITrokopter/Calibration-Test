a2a234445f44759c69d85cad1441f7655554ef0f
Sibbo <isibboi@gmail.com>
2013-11-16 18:18:15 +0100

    MAde image capturing work. Now need a chess board to find corners.

--- MyDevice.cpp
+++ MyDevice.cpp
@@ -14,7 +14,7 @@ MyDevice::MyDevice(freenect_context *_ctx, int _index)
 
 // Do not call directly even in child
 void MyDevice::VideoCallback(void* _rgb, uint32_t timestamp) {
-	std::cout << "RGB callback" << std::endl;
+	// std::cout << "RGB callback" << std::endl;
 	m_rgb_mutex.lock();
 	uint8_t* rgb = static_cast<uint8_t*>(_rgb);
 	rgbMat.data = rgb;
@@ -24,7 +24,7 @@ void MyDevice::VideoCallback(void* _rgb, uint32_t timestamp) {
 
 // Do not call directly even in child
 void MyDevice::DepthCallback(void* _depth, uint32_t timestamp) {
-	std::cout << "Depth callback" << std::endl;
+	// std::cout << "Depth callback" << std::endl;
 	m_depth_mutex.lock();
 	uint16_t* depth = static_cast<uint16_t*>(_depth);
 	depthMat.data = (uchar*) depth;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <cv.h>
+#include <highgui.h>
+#include <list>
+#include <vector>
+#include <iostream>
 
 #include "libfreenect.hpp"
 #include "MyDevice.hpp"
@@ -10,12 +14,56 @@ int main(void) {
 	cv::Mat depthMat(cv::Size(640,480), CV_16UC1);
 	cv::Mat depthf(cv::Size(640,480), CV_8UC1);
 	cv::Mat rgbMat(cv::Size(640,480), CV_8UC3, cv::Scalar(0));
-	cv::Mat ownMat(cv::Size(640,480), CV_8UC3, cv::Scalar(0));
+	
+	std::list<cv::Mat> images;
 	
 	Freenect::Freenect freenect;
 	MyDevice& device = freenect.createDevice<MyDevice>(0);
 	
-	(void) device;
+	device.startVideo();
+	
+	cv::namedWindow("rgb", CV_WINDOW_AUTOSIZE);
+	cv::namedWindow("depth", CV_WINDOW_AUTOSIZE);
+	
+	while (true) {
+		device.getVideo(rgbMat);
+		
+		// if (images.size() > 0) {
+		// 	cv::imshow("rgb", images.front());
+		// } else {
+			cv::imshow("rgb", rgbMat);
+		// }
+		
+		char key = cv::waitKey(5);
+		
+		if (key == 'x') {
+			break;
+		} else if (key == 's') {
+			images.push_back(rgbMat.clone());
+		}
+	}
+	
+	device.stopVideo();
+	
+	std::cout << "Images: " << images.size() << std::endl;
+	
+	for (std::list<cv::Mat>::iterator iter = images.begin(); iter != images.end(); ++iter) {
+		cv::imshow("rgb", *iter);
+	
+		cv::Mat greyMat(cv::Size(640, 480), CV_8UC1, cv::Scalar(0));
+	
+		cv::cvtColor(*iter, greyMat, CV_RGB2GRAY);
+		std::vector<cv::Point2f> corners;
+		
+		bool found = cv::findChessboardCorners(greyMat, cv::Size(7, 7), corners);
+		
+		std::cout << "Corners found: " << corners.size() << std::endl;
+	}
+	
+	cv::destroyWindow("depth");
+	cv::destroyWindow("rgb");
+	
+	std::cout << "Exit" << std::endl;
 	
 	return 0;
 }
